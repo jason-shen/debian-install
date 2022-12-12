c4bf22040ca9dd6ff4e8923a61f5732cc2f707c3
Jason Shen <jason_2000_nz@hotmail.com>
2022-12-12 19:36:25 +1300

    first commit

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ nala install neofetch curl flameshot psmisc mangohud vim lxappearance papirus-ic
 nala install build-essential
 echo "installing rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source "$HOME/.cargo/env" || exit
+source "home/$username/.cargo/env" || exit
 echo "install leftwm"
 cargo install leftwm
 
