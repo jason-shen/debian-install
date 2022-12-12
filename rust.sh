59e736ffb1177381f0fe71773f1a46c56b0fd03d
Jason Shen <jason_2000_nz@hotmail.com>
2022-12-12 20:03:16 +1300

    first commit

--- rust.sh
+++ rust.sh
@@ -2,4 +2,4 @@
 echo "installing rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo ".  '/home/$username/.cargo/env'" >> "/home/$username/.profile" || exit
-source "/home/$username/.profile" || exit
+source ~/.profile || exit
